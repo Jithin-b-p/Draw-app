f579006d403ddc33e502bf27eac172687b216652
unknown <jithinbp007@gmail.com>
2025-04-03 15:46:54 +0530

    added token verification on ws-server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,24 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) => {
+wss.on("connection", (socket, req) => {
+  const url = req.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") ?? "";
+
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+  if (!decoded || !decoded.userId) {
+    wss.close();
+    return;
+  }
+
   socket.on("message", (data) => {
     socket.send("pong");
   });
