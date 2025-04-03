bdd983b797498e325f29e70abd7840c14313ed8f
unknown <jithinbp007@gmail.com>
2025-04-03 19:00:59 +0530

    added common package and config

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware/middleware";
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/validation";
 
 const app = express();
 
 app.post("/signin", (req, res) => {
-  const userId = 1;
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "invalid inputs",
+    });
+    return;
+  }
 
+  const userId = 1;
   const token = jwt.sign(
     {
       userId,
@@ -18,8 +30,34 @@ app.post("/signin", (req, res) => {
   res.json({ token });
 });
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "invlid inputs",
+    });
 
-app.post("/room", middleware, (req, res) => {});
+    return;
+  }
+
+  res.json({
+    userId: 1,
+  });
+});
+
+app.post("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "invalid inputs",
+    });
+    return;
+  }
+
+  res.json({
+    name: "reees",
+  });
+});
 
 app.listen(3001);
