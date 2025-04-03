515e50a030d8b2066f6058804b74f8dde2942be3
unknown <jithinbp007@gmail.com>
2025-04-03 14:39:42 +0530

    added signin logic, middleware for token verification, corrected types

---
+++ apps/http-backend/src/middleware/middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { AuthRequest } from "../@types/express";
+
+export const middleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"] ?? "";
+
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+  if (decoded) {
+    req.userId = decoded.userId;
+    next();
+  } else {
+    res.status(403).json({
+      message: "unauthorised",
+    });
+  }
+};
